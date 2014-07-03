321efd6cdd9787919a54126986a0e9098a587d3f
hljyunxi <hljyunxi@126.com>
2014-07-03 13:10:44 +0800

    add redis lock implementation

--- src/lock.py
+++ src/lock.py
@@ -19,20 +19,44 @@ UNLOCK_LUA_SCRIPT = b"""
     end
 """
 
+UNLOCK_LUA_SCRIPT_HASH = sha1(UNLOCK_LUA_SCRIPT).hexdigest()
+
 class Lock(object):
     def __init__(self, redis_client, name, expire=None):
         self._client = redis_client
         self._name = 'lock:' + name
         self._signal = 'signal:' + name
+        self._expire = expire
         self._token = None
 
 
-    def __enter__(self):
-        pass
+    def __enter__(self, blocking = True):
+        if self._token is None:
+            self._token = urandom(16)
+        else:
+            raise RuntimeError('already acquired')
+
+        logger.info("ENTER ACQUIRE LOCK")
+
+        busy = True
+        while busy:
+            busy = not self._client.set(self._name, self._token, nx=True, ex=self._expire)
+            if busy:
+                if blocking:
+                    self._client.blpop(self._signal, self._expire or 0)
+                else:
+                    return False
+
+        return True
 
     acquire = __enter__
 
     def __exit__(self):
-        pass
+        logger.info("ENTER RELEASE LOCK")
+        try:
+            self._client.evalsha(UNLOCK_LUA_SCRIPT_HASH, 2, self._name, self._signal, self._token)
+        except NoScriptError:
+            logger.warn("UNLOCK_SCRIPT not cached.")
+            self._client.eval(UNLOCK_LUA_SCRIPT, 2, self._name, self._signal, self._token)
 
     release = __exit__
